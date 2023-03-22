2d5a9e4c4d2455581e9b95565a1ea4ba967dba7f
Padamadam <arybojad@gmail.com>
2023-03-22 22:17:25 +0100

    Attempting to fix button layout

--- maksfood/src/main/java/com/maksfood/MainWindow.java
+++ maksfood/src/main/java/com/maksfood/MainWindow.java
@@ -1,6 +1,8 @@
 package com.maksfood;
 
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicScrollBarUI;
+
 import java.awt.*;
 
 public class MainWindow extends JFrame {
@@ -14,31 +16,42 @@ public class MainWindow extends JFrame {
 
         // Create label and buttons
         JLabel label = new JLabel("Choose an option:");
+        label.setForeground(Color.WHITE);
         JButton button1 = new JButton("My Fridge");
         JButton button2 = new JButton("Recipes");
         JButton button3 = new JButton("Plan shopping");
 
         // Set button properties
-        button1.setFont(new Font("Arial", Font.PLAIN, 12));
-        button2.setFont(new Font("Arial", Font.PLAIN, 12));
-        button3.setFont(new Font("Arial", Font.PLAIN, 12));
+        button1.setFont(new Font("Arial", Font.BOLD, 12));
+        button1.setForeground(Color.WHITE);
+        button1.setBackground(new Color(80,7,2));
+        button1.setPreferredSize(new Dimension(50, 30));
 
-        button1.setPreferredSize(new Dimension(10, 10));
-        button2.setPreferredSize(new Dimension(10, 10));
-        button3.setPreferredSize(new Dimension(10, 10));
+        button2.setFont(new Font("Arial", Font.BOLD, 12));
+        button2.setForeground(Color.WHITE);
+        button2.setBackground(new Color(80,7,2));
+        button2.setPreferredSize(new Dimension(50, 30));
+
+        button3.setFont(new Font("Arial", Font.BOLD, 12));
+        button3.setForeground(Color.WHITE);
+        button3.setBackground(new Color(80,7,2));
+        button3.setPreferredSize(new Dimension(50, 30));
 
         // Create panel to hold buttons
         JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new GridLayout(3, 1, 4, 4));
+
+        buttonPanel.setBounds(61, 11, 81, 140);
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
         buttonPanel.add(button1);
         buttonPanel.add(button2);
         buttonPanel.add(button3);
+
+
         getContentPane().setBackground(new Color(82, 7, 2));
 
         // Add components to frame
-        setLayout(new BorderLayout());
-        add(label, BorderLayout.PAGE_START);
-        add(buttonPanel, BorderLayout.CENTER);
+        add(label);
+        add(buttonPanel);
 
         // Set the window to be visible
         setVisible(true);
