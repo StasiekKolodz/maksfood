4b5b23772a0ed98a79f860956b4f02bdc252a6b8
Czechos02 <01168879@pw.edu.pl>
2023-03-24 22:38:57 +0100

    frontend developing

---
+++ maksfood/src/main/java/com/maksfood/ColorButton.java
@@ -0,0 +1,44 @@
+package com.maksfood;
+
+import java.awt.Color;
+import java.awt.*;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+
+import javax.swing.JButton;
+
+public class ColorButton extends JButton {
+    private Color pressedColor = new Color(165, 56, 96);
+    private Color rolloverColor = new Color(56, 39, 57);
+    private Color normalColor = new Color(76, 59, 77);
+    private Color textColor = new Color(255, 238, 219);
+
+    public ColorButton (String text) {
+        super(text);
+        setBorderPainted(false);
+        setFocusPainted(false);
+
+        setContentAreaFilled(false);
+        setOpaque(true);
+
+        setBackground(normalColor);
+        setForeground(textColor);
+        setFont(new Font("DejaVu Sans Condensed", Font.BOLD, 18));
+        setText(text);
+
+        addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent evt) {
+                if (getModel().isPressed()) {
+                    setBackground(pressedColor);
+                } else if (getModel().isRollover()) {
+                    setBackground(rolloverColor);
+                } else {
+                    setBackground(normalColor);
+                }
+            }
+        });
+    }
+}
--- maksfood/src/main/java/com/maksfood/MainWindow.java
+++ maksfood/src/main/java/com/maksfood/MainWindow.java
@@ -1,7 +1,9 @@
 package com.maksfood;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
 // import javax.swing.plaf.basic.BasicScrollBarUI;
+import javax.swing.event.ChangeListener;
 
 import java.awt.*;
 
@@ -11,60 +13,84 @@ public class MainWindow extends JFrame {
 
         // Set window properties
         setTitle("MainWindow");
-        setSize(800, 600);
+        setSize(1200, 900);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocation(100,100);
+
         // Create label and buttons
-        // JLabel label = new JLabel("Choose an option:");
-        // label.setForeground(Color.WHITE);
-
-        JButton button1 = new JButton("My Fridge");
-        JButton button2 = new JButton("Recipes");
-        JButton button3 = new JButton("Plan shopping");
-
+        JLabel label = new JLabel("Choose an option:");
+        
+        JButton button1 = new ColorButton("My Fridge");
+        JButton button2 = new ColorButton("Recipes");
+        JButton button3 = new ColorButton("Plan shopping");
+        
+        
+        //set label properties
+        label.setForeground(new Color(165, 56, 96));
+        label.setFont(new Font("DejaVu Serif Condensed", Font.BOLD, 30));
+        
         // Set button properties
-        button1.setFont(new Font("Arial", Font.BOLD, 12));
-        button1.setForeground(Color.WHITE);
-        button1.setBackground(new Color(80,7,2));
         button1.setPreferredSize(new Dimension(200, 60));
-
-        button2.setFont(new Font("Arial", Font.BOLD, 12));
-        button2.setForeground(Color.WHITE);
-        button2.setBackground(new Color(80,7,2));
         button2.setPreferredSize(new Dimension(200, 60));
-
-        button3.setFont(new Font("Arial", Font.BOLD, 12));
-        button3.setForeground(Color.WHITE);
-        button3.setBackground(new Color(80,7,2));
         button3.setPreferredSize(new Dimension(200, 60));
-
+        
+        
+        //creating button container
         JPanel buttonPanel = new JPanel(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
-        c.insets = new Insets(10,10,10,10);
+        c.insets = new Insets(30,100,30,100);
+        c.ipadx = 20;
+        c.ipady = 20;
+        
         c.gridx = 0;
         c.gridy = 1;
         buttonPanel.add(button1,c);
+
         c.gridx = 0;
         c.gridy = 2;
         buttonPanel.add(button2,c);
+
         c.gridx = 0;
         c.gridy = 3;
         buttonPanel.add(button3,c);
+        
+        buttonPanel.setBackground(new Color(255, 238, 219));
 
-        // Create panel to hold buttons
-
-        // buttonPanel.setBounds(61, 11, 81, 140);
-        // buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
-        // buttonPanel.add(button1);
-        // buttonPanel.add(button2);
-        // buttonPanel.add(button3);
-  
+        
+        // Create label container
+        JPanel labelPanel = new JPanel(new GridLayout());
+        labelPanel.add(label);
+        labelPanel.setOpaque(false);
 
 
-        getContentPane().setBackground(new Color(82, 7, 2));
+        // Creating menu container
+        JPanel menuPanel = new RoundedPanel(new GridBagLayout(), 45);
+        menuPanel.setOpaque(false);
+        menuPanel.setBackground(new Color(255, 238, 219));
+        menuPanel.setSize(900, 1000);
 
+        GridBagConstraints d = new GridBagConstraints();
+        d.insets = new Insets(50,10,50,10);
+
+        d.gridx = 0;
+        d.gridy = 1;
+        menuPanel.add(labelPanel,d);
+
+        d.gridx = 0;
+        d.gridy = 2;
+        menuPanel.add(buttonPanel,d);
+        
+        
         // Add components to frame
-        add(buttonPanel);
+        getContentPane().setBackground(new Color(165, 56, 96));
+        setLayout(new GridBagLayout());
+        GridBagConstraints e = new GridBagConstraints();
+        e.gridheight = 900;
+        e.gridwidth = 1200;
+        e.gridx = 0;
+        e.gridy = 2;
+        add(menuPanel,e);
+
 
         // Set the window to be visible
         setVisible(true);
