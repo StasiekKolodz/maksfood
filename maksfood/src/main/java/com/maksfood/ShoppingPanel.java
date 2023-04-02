6645f40807303dc8c70a4e1ee9dd95cbe71eb833
Padamadam <arybojad@gmail.com>
2023-04-02 16:22:49 +0200

    Added new ColorButton constructor, implemented adding new list and new item to the shopping list

--- maksfood/src/main/java/com/maksfood/ShoppingPanel.java
+++ maksfood/src/main/java/com/maksfood/ShoppingPanel.java
@@ -9,9 +9,21 @@ import java.awt.event.ActionListener;
 
 public class ShoppingPanel extends RoundedPanel implements ActionListener{
 
+
+    // TODO
+    // implement class for Shopping List and List of shopping lists
+    // refactor
+
     public MainWindow window;
     public JPanel shoppingLabelPanel;
     public JPanel returnButtonPanel;
+    private ColorButton addButton;
+    private ColorButton addItemButton;
+    private ColorButton returnButton;
+    private JTextField textListField;
+    private JTextField textShoppingField;
+    public JPanel listsListPanel;
+    public JPanel shoppingListPanel;
     
     public JList<String> listsList;
     public DefaultListModel<String> listsListModel;
@@ -26,7 +38,7 @@ public class ShoppingPanel extends RoundedPanel implements ActionListener{
         DefaultLabel shoppingLabel = new DefaultLabel("Shopping");
 
         //creating return button
-        ColorButton returnButton = new ColorButton("Return");
+        returnButton = new ColorButton("Return");
         window.setButton(returnButton, this);
 
         //creating button and label panels
@@ -39,10 +51,41 @@ public class ShoppingPanel extends RoundedPanel implements ActionListener{
         setOpaque(false);
         setBackground(new Color(255, 238, 219));
 
-        //adding elements to RecipesPanel
-        
-        formListOfLists();
+
+        //forming both list of lists and shopping list
+        /* 
+        TODO 
+        refactor the function, so it sets only one list, which is passed as an argument
+        */
+        formLists();
+
+
+        // setting add button both for list of lists and shopping list
+        /* 
+        TODO 
+        refactor the function, so it sets only one button, a list is passed as an argument
+        */
+        setAddButton(this);
+
+
+        // setting layout of the panel
+        /* 
+        TODO 
+        fix allignment for desired look:
+                | Shopping |
+            -----------------------
+            List 1    |     List 2
+            -----------------------
+                |  Return  |
+        */
         setLayout();
+
+
+        // setting appearance of lists, so it corresponds to the app graphics
+        /* 
+        TODO 
+        set the visible frames of the list
+        */
         setListAppearance(listsList);
         setListAppearance(shoppingList);
 
@@ -50,13 +93,6 @@ public class ShoppingPanel extends RoundedPanel implements ActionListener{
     }
 
     public void setLayout(){
-
-        // example list
-        // String foo[] = {"Eggs", "Milk"};
-        // JList<String> shoppingList = new JList<>(foo);
-        // shoppingList.setFixedCellHeight(25);
-        // shoppingList.setFixedCellWidth(200);
-
 
         GridBagConstraints e = new GridBagConstraints();
         e.insets = new Insets(50,10,50,10);
@@ -66,19 +102,33 @@ public class ShoppingPanel extends RoundedPanel implements ActionListener{
         
         e.gridx = 0;
         e.gridy = 2;
-        this.add(listsList, e);
+        this.add(listsListPanel, e);
 
         e.gridx = 2;
         e.gridy = 2;
-        this.add(shoppingList, e);
+        this.add(shoppingListPanel, e);
         
         e.gridx = 1;
         e.gridy = 3;
         this.add(returnButtonPanel, e);
     }
 
-    public void formListOfLists(){
+    public void formLists(){
+        listsListPanel = new JPanel();
+        listsListPanel.setLayout(new BorderLayout());
+
+        shoppingListPanel = new JPanel();
+        shoppingListPanel.setLayout(new BorderLayout());
+
+        textListField = new JTextField();
+        textShoppingField = new JTextField();
+
         listsListModel = new DefaultListModel<>();
+
+        /* 
+        TODO 
+        add database for shopping lists collection
+        */
         listsListModel.addElement("Tortilla");
         listsListModel.addElement("StudentPack");
         listsListModel.addElement("Harcerz");
@@ -88,6 +138,10 @@ public class ShoppingPanel extends RoundedPanel implements ActionListener{
         listsList = new JList<>(listsListModel);
         shoppingList = new JList<>(shoppingListModel);
 
+        /* 
+        TODO 
+        refactor the function
+        */
         listsList.addListSelectionListener(new ListSelectionListener() {
             @Override
             public void valueChanged(ListSelectionEvent e) {
@@ -104,8 +158,23 @@ public class ShoppingPanel extends RoundedPanel implements ActionListener{
                 }
             }
         });
-        
+
+        listsListPanel.add(listsList, BorderLayout.NORTH);
+        listsListPanel.add(textListField, BorderLayout.SOUTH);
+
+        shoppingListPanel.add(shoppingList, BorderLayout.NORTH);
+        shoppingListPanel.add(textShoppingField, BorderLayout.SOUTH);
     }
+
+    public void setAddButton(ActionListener l){
+        addButton = new ColorButton("Add item", 15);
+        addItemButton = new ColorButton("Add item", 15);
+        listsListPanel.add(addButton, BorderLayout.EAST);
+        shoppingListPanel.add(addItemButton, BorderLayout.WEST);
+        addButton.addActionListener(l);
+        addItemButton.addActionListener(l);
+    }
+
 
     public void setListAppearance(JList list){
         list.setForeground(new Color(165, 56, 96));
@@ -115,6 +184,11 @@ public class ShoppingPanel extends RoundedPanel implements ActionListener{
     }
     // returns a list of items for the given list name
     private static String[] getListItems(String listName) {
+        /* 
+        TODO 
+        add database for shopping list items
+        preferably in separete ShoppingList class
+        */
         if (listName.equals("Tortilla")) {
             return new String[] {"Tortilla", "Lettuce Mix", "Roasted Chicken", "Garlic Sauce", "Sriracha Sauce"};
         } else if (listName.equals("StudentPack")) {
@@ -128,12 +202,27 @@ public class ShoppingPanel extends RoundedPanel implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+        Object src = e.getSource();
+
+        if(src == returnButton){
         window.currentPanel.setVisible(false);
-
         window.currentPanel = window.menuPanel;
-
         window.currentPanel.setVisible(true);
+        }
+        else if(src == addButton){
+            String text = textListField.getText();
+                if (!text.isEmpty()) {
+                    listsListModel.addElement(text);
+                    textListField.setText("");
+                }
+        }
+        else if(src == addItemButton){
+            String text = textShoppingField.getText();
+                if (!text.isEmpty()) {
+                    shoppingListModel.addElement(text);
+                    textShoppingField.setText("");
+                }
+        }
     }
 
 
