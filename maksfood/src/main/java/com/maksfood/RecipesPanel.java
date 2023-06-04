b844acb85211351ae0e8b0ebb3ce07f1ff2456c5
Czechos02 <01168879@pw.edu.pl>
2023-06-04 17:06:29 +0200

    deleting useless import

--- maksfood/src/main/java/com/maksfood/RecipesPanel.java
+++ maksfood/src/main/java/com/maksfood/RecipesPanel.java
@@ -1,8 +1,6 @@
 package com.maksfood;
 
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 import javax.swing.plaf.basic.BasicScrollBarUI;
 
 import org.json.JSONArray;
